50c48d04f86f0e2907298c2c57ad2871e2583a7c
yoga sukma <yogasukma@runcloud.io>
2022-10-20 10:57:16 +0800

    php

---
+++ index.php
@@ -0,0 +1,9 @@
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+body {
+    text-align: center;
+    font-family: 'Bebas Neue', cursive;
+    margin-top: 30%;
+}
+</style>
+<h1>Hello JomLaunch 🚀</h1>
